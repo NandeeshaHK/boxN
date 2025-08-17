b4f5d1cb22bba319c9bb40d4ae7f5a0b27df5a99
NandeeshaHK <nandeeshahk875@gmail.com>
2025-08-17 18:31:02 +0530

    chore: pre-deploy commit

--- deploy-page-git.sh
+++ deploy-page-git.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Configuration
 BUILD_DIR="site"       # local folder containing static site to publish
 REMOTE="origin"        # The name of the remote repository (usually "origin")
-TARGET_BRANCH="master" # The branch to push the subtree to
+TARGET_BRANCH="gh-pages" # The branch to push the subtree to
 
 if [ ! -d "$BUILD_DIR" ]; then
   echo "Error: build dir '$BUILD_DIR' not found. Put your static files there."
